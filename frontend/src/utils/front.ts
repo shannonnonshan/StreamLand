42e43d25c333496979d790c8535f3cd655e25fc8
shannonnonshan <khanhminhdoan04@gmail.com>
2025-09-16 10:24:33 +0700

    font upd

---
+++ frontend/src/utils/front.ts
@@ -0,0 +1,7 @@
+import { Raleway } from "next/font/google";
+
+export const raleway = Raleway({
+  variable: "--font-raleway",
+  weight: ["100","200","300","400","500","600","700", "800", "900"],
+  subsets: ["latin"],
+});
